81753c580d34a36e85a8bdb2072cd0963a3fc47d
Sergio Torres Guaymasi <92409487+Sergio-TG@users.noreply.github.com>
2024-08-27 21:46:16 +0000

    Crear workflow

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,17 @@
-// Placeholder to host a component
+// Hours.js
+import React from 'react';
+
+const Hours = () => {
+  // Lógica para determinar las horas actuales y si el refugio está abierto
+  // ...
+
+  return (
+    <div>
+      <h2>Horario del refugio hoy:</h2>
+      <p>De lunes a viernes: 10 a.m. - 4 p.m.</p>
+      <p>Sábados y domingos: 9 a.m. - 8 p.m.</p>
+    </div>
+  );
+};
+
+export default Hours;
